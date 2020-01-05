9c15f0ca6cad66cac4ec9a044afea249e2f635a5
unknown <darques@cifpfbmoll.eu>
2020-01-05 12:34:45 +0100

    refactor(main):Cambiar nombres variables de los items

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ from BackstagePass import BackstagePass
 
 def main(passedDays):
 
-    AgedBrie0 = AgedBrie("Aged Brie", 2, 0)
-    Sulfuras0 = Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80)
-    Sulfuras1 = Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80)
-    Dexterity0 = Dexterity("+5 Dexterity Vest", 10, 20)
-    Elixir0 = Elixir("Elixir of Mongoose", 5, 7)
-    Conjured0 = Conjured("Conjured Mana Cake", 3, 6)
-    BackstagePass0 = BackstagePass("Backstage Passes to TAFKAL80ETC concert", 15, 20)
-    BackstagePass1 = BackstagePass("Backstage Passes to TAFKAL80ETC concert", 10, 49)
-    BackstagePass2 = BackstagePass("Backstage Passes to TAFKAL80ETC concert", 5, 49)
+    agedBrie = AgedBrie("Aged Brie", 2, 0)
+    sulfuras = Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80)
+    sulfuras1 = Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80)
+    dexterity = Dexterity("+5 Dexterity Vest", 10, 20)
+    elixirMongoose = Elixir("Elixir of Mongoose", 5, 7)
+    conjuredManaCake = Conjured("Conjured Mana Cake", 3, 6)
+    backstagePass = BackstagePass("Backstage Passes to TAFKAL80ETC concert", 15, 20)
+    backstagePass1 = BackstagePass("Backstage Passes to TAFKAL80ETC concert", 10, 49)
+    backstagePass2 = BackstagePass("Backstage Passes to TAFKAL80ETC concert", 5, 49)
 
-    listaInventario = [AgedBrie0, Sulfuras0, Sulfuras1, Dexterity0, Elixir0, Conjured0, BackstagePass0, BackstagePass1, BackstagePass2]
+    listaInventario = [agedBrie, sulfuras, sulfuras1, dexterity, elixirMongoose, conjuredManaCake, backstagePass, backstagePass1, backstagePass2]
     tienda = GilderRose(listaInventario)
     for day in range(1, passedDays+1):
         print('\n' + '----------' + str(day) + '----------')
