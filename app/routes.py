c4397204cc4069a8e5894fc7e2c87a69cbdb8438
DanyZurita <dzurita@cifpfbmoll.eu>
2020-01-07 05:04:05 +0100

    style(flask): Usar bootstrap para cada template

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,25 @@
-from app import app
+from app import app, bootstrap
 from flask import Flask, render_template
 from GildedRose import *
 
+
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 
 @app.route('/update')
 def update():
     return '<h1>Bad Request</h1>', 400
 
+
 @app.route('/update/<day>')
 def update_day(day):
     if not day:
         abort(404)
-    return render_template('update.html', days=day, main=main)
+    return render_template('update.html', days=day)
