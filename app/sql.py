324ce8d7b2c80a9476ae4bd1b8619eb717754dda
DanyZurita <dzurita@cifpfbmoll.eu>
2020-01-08 08:09:52 +0100

    feat(sql): Añadir creación de sql con SQLAlquemy

---
+++ app/sql.py
@@ -0,0 +1,22 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class GildedRose(db.Model):
+    __tablename__ = 'gildedrose'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    sellin = db.Column(db.String(64), unique=True)
+    quality = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Stock: %r>' % (self.name, self.sellin, self.quality)
