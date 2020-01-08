5679cd0ff116ec29ac372a504d2243039d7aeb99
unknown <darques@cifpfbmoll.eu>
2020-01-08 11:45:38 +0100

    feat(accesoCasosTest): Leer archivo casos test

--- GildedRose/accesoCasosTest.py
+++ GildedRose/accesoCasosTest.py
@@ -1,8 +1,16 @@
 def readTestingFile(filePath):
     testFile = open(filePath, 'r')
     testArray = []
+    acumulador = []
     for line in testFile:
         if line.find('day') != -1:
+            testArray.append(acumulador)
+            acumulador = []
         if line.find('day') and line.find('name') and line.find('sellIn') and line.find('quality') == -1:
             item = line.rsplit(',')
-            testArray.append(item)
+            acumulador.append(item)
+
+    testFile.close()
+    return testArray
+
+print(readTestingFile('stdout.gr'))
