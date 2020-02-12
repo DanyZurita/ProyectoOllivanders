e464ef6ca02de55021375d6955e9858d6fc4a8cb
DanyZurita <dzurita@cifpfbmoll.eu>
2020-02-12 20:10:48 +0100

    fix(init): Quitar comentarios merge

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,7 @@
 from flask import Flask
-<<<<<<< HEAD
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/index.html')
-
-def index():
-    return "Hello World!"
-=======
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
->>>>>>> a6fea659ffd9a7f9264ac3b72f0373ace29301a0
